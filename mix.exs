0e615a63a511ef594bd892aba231ceca0e2d6a8a
Attila Sragli <sragli.attila@gmail.com>
2025-08-08 21:43:52 +0200

    Updated ExDoc generation, docs formatting, renamed Livebook

--- mix.exs
+++ mix.exs
@@ -11,11 +11,11 @@ defmodule CCM.MixProject do
       package: package(),
       deps: deps(),
       name: "CCM",
-      source_url: "https://github.com/sragli/ccm"
+      source_url: "https://github.com/sragli/ccm",
+      docs: docs()
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
@@ -28,18 +28,22 @@ defmodule CCM.MixProject do
 
   defp package() do
     [
-      # These are the default files included in the package
       files: ~w(lib .formatter.exs mix.exs README.md LICENSE CHANGELOG),
       licenses: ["Apache-2.0"],
       links: %{"GitHub" => "https://github.com/sragli/ccm"}
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
+  defp docs() do
+    [
+      main: "CCM",
+      extras: ["README.md", "LICENSE", "examples.livemd"]
+    ]
+  end
+
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
     ]
   end
 end
